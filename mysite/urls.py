aacbc70eeaf288a5bffc3a42f6be876fd6ee3cc3
BERNADINE AMAKA EMMANUEL <b.emmanuel.22@abdn.ac.uk>
2023-05-07 02:18:36 +0000

    trying more changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,12 +18,17 @@ from django.urls import include, path
 from django.views.generic import RedirectView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path, include, re_path
+from django.views.static import serve
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls',  namespace='shop')),
     path('', RedirectView.as_view(url='')),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
